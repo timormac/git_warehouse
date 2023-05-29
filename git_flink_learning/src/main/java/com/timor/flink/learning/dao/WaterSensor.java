1b16256940f7cff69e07bbfeb28f492f2dcb4e43
timormac <2867102374@qq.com>
2023-05-29 09:35:47 +0800

    增加了WaterSensor和GetSourc,pom改变

---
+++ git_flink_learning/src/main/java/com/timor/flink/learning/dao/WaterSensor.java
@@ -0,0 +1,73 @@
+package com.timor.flink.learning.dao;
+
+import java.util.Objects;
+
+/**
+ * @Title: WaterSensor
+ * @Package: com.timor.flink.learning.dao
+ * @Description:
+ * @Author: XXX
+ * @Date: 2023/5/29 09:15
+ * @Version:1.0
+ */
+public class WaterSensor {
+
+    private  String id ;
+    private  Long  ts;
+    private  Integer vc;
+
+    public WaterSensor() {
+    }
+
+    public WaterSensor(String id, Long ts, Integer vc) {
+        this.id = id;
+        this.ts = ts;
+        this.vc = vc;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Long getTs() {
+        return ts;
+    }
+
+    public Integer getVc() {
+        return vc;
+    }
+
+    public void setVc(Integer vc) {
+        this.vc = vc;
+    }
+
+    public void setTs(Long ts) {
+        this.ts = ts;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        WaterSensor that = (WaterSensor) o;
+        return Objects.equals(id, that.id) && Objects.equals(ts, that.ts) && Objects.equals(vc, that.vc);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, ts, vc);
+    }
+
+    @Override
+    public String toString() {
+        return "WaterSensor{" +
+                "id='" + id + '\'' +
+                ", ts=" + ts +
+                ", vc=" + vc +
+                '}';
+    }
+}
---
+++ git_flink_learning/src/main/java/com/timor/flink/learning/source/A2_KafkaSourceDemo.java
@@ -0,0 +1,45 @@
+package com.timor.flink.learning.source;
+
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.connector.kafka.source.KafkaSource;
+import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+/**
+ * @Title: A2_KafkaSourceDemo
+ * @Package: com.timor.flink.learning.source
+ * @Description:
+ * @Author: XXX
+ * @Date: 2023/5/29 09:32
+ * @Version:1.0
+ */
+public class A2_KafkaSourceDemo {
+
+    public static void main(String[] args) throws Exception {
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        //从kafka获取
+        KafkaSource<String> kafkaSource = KafkaSource.<String>builder() //builder前面要指定范型方法
+                .setBootstrapServers("project1:9092,project2:9092,project3:9092")
+                .setGroupId("aaa")
+                .setTopics("flink_word")
+                .setValueOnlyDeserializer(new SimpleStringSchema())//指定从kafka获取的数据的反序列化器
+                .setStartingOffsets(OffsetsInitializer.latest())  //设置消费起始offset
+                .build();
+
+        //下面这个是kafka的auto.reset.offsets
+        //earlist:如果有offset,从offset继续消费，没有从最早消费
+        //lastest:如果有offset,从offset继续消费，没有从最新消费
+
+        //flink的offset和kafka的不同，earlist一定从最早，latest一定从最新，不管有没有offset
+
+        DataStreamSource<String> ds = env.fromSource(kafkaSource, WatermarkStrategy.noWatermarks(), "kafkasource");
+
+        ds.print();
+
+        env.execute();
+    }
+}
